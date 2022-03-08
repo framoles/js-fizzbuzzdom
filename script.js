d81c25e339525445cc9fb409471f57f9b763a2d5
Francesco Moles <francesco.moles@outlook.it>
2022-03-08 16:12:54 +0100

    milestone 2

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
+const squares = document.querySelector("#squares");
+
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0) {
-        console.log("fizz")
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        const squareDiv = `<div class="square">${"FizzBuzz"}</div>`;
+        squares.innerHTML += squareDiv;
+    }
+    else if (i % 3 === 0) {
+        const squareDiv = `<div class="square">${"Fizz"}</div>`;
+        squares.innerHTML += squareDiv;
     }
     else if (i % 5 === 0) {
-        console.log("buzz")
+        const squareDiv = `<div class="square">${"Buzz"}</div>`;
+        squares.innerHTML += squareDiv;
     }
     else {
-        console.log(i);
+        const squareDiv = `<div class="square">${i}</div>`;
+        squares.innerHTML += squareDiv;
     }
 }
